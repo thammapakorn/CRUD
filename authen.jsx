8c98cff0c3e4d3cc608192f8a9df14ff12b40cb8
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2024-06-24 18:16:11 +0700

    Delete authen.jsx

--- authen.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-//http://localhost:5000/api/login
-
-export const register = async (data) =>
-  await axios.post('http://localhost:5000/api/register/',data);
-
-export const login = async (data) =>
-  await axios.post('http://localhost:5000/api/login/',data);
